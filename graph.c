2feb8cb1a3a636706a20206d10e78ee5fedcba1f
skymkmk <admin@skymkmk.com>
2023-11-03 18:46:09 +0800

    add the defination of graph

---
+++ graph.c
@@ -0,0 +1,63 @@
+#include "graph.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+VNode *newVertices(const int length, const int data[]) {
+    VNode * newVertice = (VNode *)malloc(sizeof(VNode) * length);
+    for (int i = 0; i < length; i++) {
+        newVertice[i].data = data[i];
+        newVertice[i].first = NULL;
+    }
+    return newVertice;
+}
+
+void setEdge(VNode *const vertices, const int vertice, const int adjvex) {
+    ENode* node = (ENode*)malloc(sizeof(ENode));
+    node->adjvex = adjvex;
+    node->next = NULL;
+    if (vertices[vertice].first == NULL) vertices[vertice].first = node;
+    else {
+        ENode* p = vertices[vertice].first;
+        while (p->next != NULL) p = p-> next;
+        p->next = node;
+    }
+}
+
+graph generateGraphFromFile(const char filename[]) {
+    FILE* fp = fopen(filename, "r");
+    int length;
+    fscanf(fp, "%d", &length);
+    int *data = (int*)malloc(sizeof(int) * length);
+    for (int i = 0; i < length; i++) fscanf(fp, "%d", &data[i]);
+    VNode * vertices = newVertices(length, data);
+    int adjvex;
+    char c;
+    for (int i = 0; i < length; i++) {
+        while (1) {
+            fscanf(fp, "%d", &adjvex);
+            setEdge(vertices, i, adjvex);
+            c = fgetc(fp);
+            if (c == '\n' || c == EOF) break;
+        }
+    }
+    fclose(fp);
+    graph freshGraph;
+    freshGraph.length = length;
+    freshGraph.vertices = vertices;
+    return freshGraph;
+}
+
+void printGraph (const graph theGrpah) {
+    for (int i = 0; i < theGrpah.length; i++) {
+        printf("%d ", theGrpah.vertices[i].data);
+        if (theGrpah.vertices[i].first == NULL) printf("\n");
+        else {
+            ENode* p = theGrpah.vertices[i].first;
+            do {
+                printf("-> %d ", p->adjvex);
+                p = p->next;
+            } while (p != NULL);
+        }
+        printf("\n");
+    }
+}
---
+++ graph.h
@@ -0,0 +1,29 @@
+// 使用邻接表法
+
+// 边节点
+typedef struct ENode {
+    int adjvex; // 该边在顶点表中的位置
+    struct ENode *next; // 下一个边
+} ENode;
+
+// 顶点节点
+typedef struct VNode {
+    int data; // 顶点数据
+    ENode *first; // 第一个相连的边
+} VNode;
+
+typedef struct graph {
+    VNode* vertices;
+    int length;
+} graph;
+
+// 生成顶点表
+VNode * newVertices(const int length, const int data[]); // length 为顶点数量，data 数组为其内容
+
+// 设置边
+void setEdge(VNode *const vertices, const int vertice, const int adjvex); // vertices 为顶点表，vertice 为顶点，adjvex 为邻接顶点
+
+// 从文件输入图
+graph generateGraphFromFile(const char filename[]);
+
+void printGraph (const graph theGrpah);
